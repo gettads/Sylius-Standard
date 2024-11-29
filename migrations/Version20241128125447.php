90b2b740a180aea6d30c79859a28f86c65155539
Dmitrii Getta <dmitrii.getta@klikpojisteni.cz>
2024-11-29 16:27:37 +0100

    product settings bundle integration

---
+++ migrations/Version20241128125447.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241128125447 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_product ADD promo_quantity_multiplier INT DEFAULT 7 NOT NULL');
+        $this->addSql('ALTER TABLE sylius_product_variant ADD quantity_multiplier INT DEFAULT 10 NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_product DROP promo_quantity_multiplier');
+        $this->addSql('ALTER TABLE sylius_product_variant DROP quantity_multiplier');
+    }
+}
--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace App\Entity\Product;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gtt\SyliusProductSettingPlugin\Contract\Product\ProductQuantityMultiplierPromoAwareInterface;
+use Gtt\SyliusProductSettingPlugin\Contract\Product\ProductQuantityMultiplierPromoAwareTrait;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_product')]
-class Product extends BaseProduct
+class Product extends BaseProduct implements ProductQuantityMultiplierPromoAwareInterface
 {
+    use ProductQuantityMultiplierPromoAwareTrait;
+
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
